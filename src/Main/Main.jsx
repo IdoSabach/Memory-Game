5071e599b4f05e9bf50a04ad472612e8b4aa1124
Ido <Idosabach11@gmail.com>
2024-02-21 22:12:52 +0200

    import image

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,6 +1,17 @@
 import Card from "../Card/Card";
 
-import  blackBlood from "../images/icons8-black-blood-96.png"
+import blackBlood from "../images/icons8-black-blood-96.png"
+import bmo from "../images/icons8-bmo-96.png"
+import finn from "../images/icons8-finn-96.png"
+import gunter from "../images/icons8-gunter-96.png"
+import hammerstein from "../images/icons8-hammerstein-96.png"
+import iceKing from "../images/icons8-ice-king-96.png"
+import jake from "../images/icons8-jake-96.png"
+import pineapples from "../images/icons8-joe-pineapples-96.png"
+import marceline from "../images/icons8-marceline-96.png"
+import mek from "../images/icons8-mek-quake-96.png"
+import mongrol from "../images/icons8-mongrol-96.png"
+import princess from "../images/icons8-princess-bubblegum-96.png"
 
 export default function Main(){
   return(
