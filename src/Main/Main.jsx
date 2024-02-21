381ccb36f0c7ec0cb87cae313a2526df49a044dc
Ido <Idosabach11@gmail.com>
2024-02-21 17:40:50 +0200

    add card and main

---
+++ src/Main/Main.jsx
@@ -0,0 +1,11 @@
+import Card from "../Card/Card";
+
+import  blackBlood from "../images/icons8-black-blood-96.png"
+
+export default function Main(){
+  return(
+    <div className="Main w-14">
+      <Card image={blackBlood} text={"Black Blood"}/>
+    </div>
+  )
+}
