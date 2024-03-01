ee94baf6a387484d7426afe632017ae3f92df0aa
Ido <Idosabach11@gmail.com>
2024-03-01 13:41:48 +0200

    fix and finish the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
-// import backImage from './images/wallpaperflare.com_wallpaper (3).jpg'
 import './App.css'
 
 export default function App() {
@@ -14,11 +13,3 @@ export default function App() {
       
   );
 }
-
-/*
--app
--header
--main
---card
--footer
-*/
